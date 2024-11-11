06af6b7ef18ec4940697ba25bfdd8100e72001c4
Saleep Shrestha <saleep24@gmail.com>
2024-11-11 13:25:27 -0600

    Updates

---
+++ static/js/952.20fb0b16.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhealthcheck=self.webpackChunkhealthcheck||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.20fb0b16.chunk.js.map
